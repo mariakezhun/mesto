4f18a0d4bdee0b12e512ef1d68efa17147d129f9
Maria Kezhun <marusa.kezhoon@yandex.ru>
2023-01-09 23:26:22 +0300

    09.01.23 5

--- scripts/index.js
+++ scripts/index.js
@@ -3,32 +3,29 @@ let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description'); 
-
-let popupOpen = function() {
-  popup.classList.add('popup__opened')
-  nameInput.value = profileName.textContent
-  jobInput.value = profileJob.textContent
-};
-
-popupOpenButton.addEventListener('click', popupOpen);
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__text_type_name');
 let jobInput = formElement.querySelector('.popup__text_type_job');
 
-nameInput.value =  profileName.textContent;
+let popupOpen = function() {
+  popup.classList.add('popup_opened')
+  nameInput.value = profileName.textContent
+  jobInput.value = profileJob.textContent
+};
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 }
+
+let popupClose = function(){
+  popup.classList.remove('popup_opened')
+};
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-let popupClose = function(){
-  popup.classList.remove('popup__opened')
-};
+popupOpenButton.addEventListener('click', popupOpen);
 
 popupCloseButton.addEventListener('click',popupClose);
